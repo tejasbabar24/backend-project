e591625df55274e81c4f69648b0361e41a3c6cf3
Tejasbabar24 <tejasbabar864@gmail.com>
2024-12-29 20:56:23 +0530

    asyncHandler   ApiError and ApiResponse created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path : './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from './app.js'
 
 dotenv.config({
    path : './env' 
@@ -8,3 +9,17 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3001, ()=>{
+        console.log(` server is running at  PORT : ${process.env.PORT || 3001}`);
+        
+    })
+    app.on('error' , (error)=>{
+        console.log("Error :",error);
+        throw error       
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed  !!!" , err);
+    
+})
